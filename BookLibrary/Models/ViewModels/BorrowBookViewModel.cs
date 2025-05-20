af57d61dce9396f660252744f437d694e16e544c
Михаил Голубь <golubmichael060602@gmail.com>
2025-05-20 10:54:33 +0300

    Add Controls and VieModels

---
+++ BookLibrary/Models/ViewModels/BorrowBookViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BookLibrary.Models.ViewModels
+{
+    public class BorrowBookViewModel
+    {
+        [Required]
+        public Guid ReaderId { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, выберите книгу.")]
+        [Display(Name = "Книга для выдачи")]
+        public Guid SelectedBookId { get; set; }
+
+        public IEnumerable<SelectListItem>? AvailableBooks { get; set; } // Список доступных книг
+        public string? ReaderName { get; set; } // Для отображения имени читателя на форме
+    }
+}
---
+++ BookLibrary/Models/ViewModels/ReaderViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using BookLibrary.Models; // Для списка книг
+
+namespace BookLibrary.Models.ViewModels
+{
+    public class ReaderViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Имя читателя обязательно для заполнения")]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Фамилия читателя обязательна для заполнения")]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Отчество")]
+        public string? MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Email обязателен для заполнения")]
+        [EmailAddress(ErrorMessage = "Некорректный формат Email")]
+        public string Email { get; set; }
+
+        [Phone(ErrorMessage = "Некорректный формат телефона")]
+        [Display(Name = "Телефон")]
+        public string? PhoneNumber { get; set; }
+
+        [Display(Name = "Дата регистрации")]
+        public DateTime RegistrationDate { get; set; }
+
+        public string FullName => $"{LastName} {FirstName} {MiddleName}".Trim();
+
+        public IEnumerable<BookWithAuthorsViewModel>? BorrowedBooks { get; set; } // Книги, взятые читателем
+    }
+
+    // Дополнительная ViewModel для отображения книг с авторами
+    public class BookWithAuthorsViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public List<string> AuthorNames { get; set; } = new List<string>();
+        public string? CurrentReaderName { get; set; } // Если нужно показать, кто взял
+        public Guid? CurrentReaderId { get; set; }
+    }
+}
